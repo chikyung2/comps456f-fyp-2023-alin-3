98d07d7cfac1cb09b1c24cfb18e61c62a688ac02
chikyung2 <chikyung2@gmail.com>
2024-03-20 02:50:43 +0800

    create frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button>Testing</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
